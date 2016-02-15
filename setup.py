24dbf3c0ebbc28b73f7674713d5a6eb1a89275cc
Kernc <kerncece@gmail.com>
2016-02-15 14:03:15 +0100

    Don't overwrite /etc/rawzica.conf

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 
+import os
 import sys
+from shutil import copy
 
 if sys.version_info.major < 3:
     raise RuntimeError('Need Python 3 (or later) for rawzica!')
@@ -25,4 +27,6 @@ setup(
         ('/etc/', ['rawzica.conf']),
     ),
 )
-
+ETC_FILE = 'rawzica.conf'
+if not os.path.exists('/etc/' + ETC_FILE):
+    copy(os.path.join(__file__, ETC_FILE), '/etc/')
